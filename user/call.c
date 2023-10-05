ffb52708fa41839642841a39f1cb6c94472040d5
tianzhenxiong <2096067752@qq.com>
2023-10-05 13:56:19 +0000

    finish backtrace

--- user/call.c
+++ user/call.c
@@ -3,15 +3,12 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int g(int x) {
-  return x+3;
-}
+int g(int x) { return x + 3; }
 
-int f(int x) {
-  return g(x);
-}
+// no inline
+int f(int x) { return g(x); }
 
 void main(void) {
-  printf("%d %d\n", f(8)+1, 13);
+  printf("%d %d\n", f(8) + 1, 13);
   exit(0);
 }
