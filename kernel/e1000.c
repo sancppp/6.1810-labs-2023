5cd1b5b65b80e98c29c44660cfa57192d8ed52fd
tianzhenxiong <2096067752@qq.com>
2023-10-26 04:55:32 +0000

    finish net lab

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -19,24 +19,26 @@ static struct mbuf *rx_mbufs[RX_RING_SIZE];
 // remember where the e1000's registers live.
 static volatile uint32 *regs;
 
-struct spinlock e1000_lock;
+// struct spinlock e1000_lock;
+struct spinlock e1000_lock_rx;
+struct spinlock e1000_lock_tx;
 
 // called by pci_init().
 // xregs is the memory address at which the
 // e1000's registers are mapped.
-void
-e1000_init(uint32 *xregs)
-{
+void e1000_init(uint32 *xregs) {
   int i;
 
-  initlock(&e1000_lock, "e1000");
+  // initlock(&e1000_lock, "e1000");
+  initlock(&e1000_lock_rx, "e1000_rx");
+  initlock(&e1000_lock_tx, "e1000_tx");
 
   regs = xregs;
 
   // Reset the device
-  regs[E1000_IMS] = 0; // disable interrupts
+  regs[E1000_IMS] = 0;  // disable interrupts
   regs[E1000_CTL] |= E1000_CTL_RST;
-  regs[E1000_IMS] = 0; // redisable interrupts
+  regs[E1000_IMS] = 0;  // redisable interrupts
   __sync_synchronize();
 
   // [E1000 14.5] Transmit initialization
@@ -45,56 +47,50 @@ e1000_init(uint32 *xregs)
     tx_ring[i].status = E1000_TXD_STAT_DD;
     tx_mbufs[i] = 0;
   }
-  regs[E1000_TDBAL] = (uint64) tx_ring;
-  if(sizeof(tx_ring) % 128 != 0)
-    panic("e1000");
+  regs[E1000_TDBAL] = (uint64)tx_ring;
+  if (sizeof(tx_ring) % 128 != 0) panic("e1000");
   regs[E1000_TDLEN] = sizeof(tx_ring);
   regs[E1000_TDH] = regs[E1000_TDT] = 0;
-  
+
   // [E1000 14.4] Receive initialization
   memset(rx_ring, 0, sizeof(rx_ring));
   for (i = 0; i < RX_RING_SIZE; i++) {
     rx_mbufs[i] = mbufalloc(0);
-    if (!rx_mbufs[i])
-      panic("e1000");
-    rx_ring[i].addr = (uint64) rx_mbufs[i]->head;
+    if (!rx_mbufs[i]) panic("e1000");
+    rx_ring[i].addr = (uint64)rx_mbufs[i]->head;
   }
-  regs[E1000_RDBAL] = (uint64) rx_ring;
-  if(sizeof(rx_ring) % 128 != 0)
-    panic("e1000");
+  regs[E1000_RDBAL] = (uint64)rx_ring;
+  if (sizeof(rx_ring) % 128 != 0) panic("e1000");
   regs[E1000_RDH] = 0;
   regs[E1000_RDT] = RX_RING_SIZE - 1;
   regs[E1000_RDLEN] = sizeof(rx_ring);
 
   // filter by qemu's MAC address, 52:54:00:12:34:56
   regs[E1000_RA] = 0x12005452;
-  regs[E1000_RA+1] = 0x5634 | (1<<31);
+  regs[E1000_RA + 1] = 0x5634 | (1 << 31);
   // multicast table
-  for (int i = 0; i < 4096/32; i++)
-    regs[E1000_MTA + i] = 0;
+  for (int i = 0; i < 4096 / 32; i++) regs[E1000_MTA + i] = 0;
 
   // transmitter control bits.
-  regs[E1000_TCTL] = E1000_TCTL_EN |  // enable
-    E1000_TCTL_PSP |                  // pad short packets
-    (0x10 << E1000_TCTL_CT_SHIFT) |   // collision stuff
-    (0x40 << E1000_TCTL_COLD_SHIFT);
-  regs[E1000_TIPG] = 10 | (8<<10) | (6<<20); // inter-pkt gap
+  regs[E1000_TCTL] = E1000_TCTL_EN |                  // enable
+                     E1000_TCTL_PSP |                 // pad short packets
+                     (0x10 << E1000_TCTL_CT_SHIFT) |  // collision stuff
+                     (0x40 << E1000_TCTL_COLD_SHIFT);
+  regs[E1000_TIPG] = 10 | (8 << 10) | (6 << 20);  // inter-pkt gap
 
   // receiver control bits.
-  regs[E1000_RCTL] = E1000_RCTL_EN | // enable receiver
-    E1000_RCTL_BAM |                 // enable broadcast
-    E1000_RCTL_SZ_2048 |             // 2048-byte rx buffers
-    E1000_RCTL_SECRC;                // strip CRC
-  
+  regs[E1000_RCTL] = E1000_RCTL_EN |       // enable receiver
+                     E1000_RCTL_BAM |      // enable broadcast
+                     E1000_RCTL_SZ_2048 |  // 2048-byte rx buffers
+                     E1000_RCTL_SECRC;     // strip CRC
+
   // ask e1000 for receive interrupts.
-  regs[E1000_RDTR] = 0; // interrupt after every received packet (no timer)
-  regs[E1000_RADV] = 0; // interrupt after every packet (no timer)
-  regs[E1000_IMS] = (1 << 7); // RXDW -- Receiver Descriptor Write Back
+  regs[E1000_RDTR] = 0;  // interrupt after every received packet (no timer)
+  regs[E1000_RADV] = 0;  // interrupt after every packet (no timer)
+  regs[E1000_IMS] = (1 << 7);  // RXDW -- Receiver Descriptor Write Back
 }
 
-int
-e1000_transmit(struct mbuf *m)
-{
+int e1000_transmit(struct mbuf *m) {
   //
   // Your code here.
   //
@@ -102,24 +98,63 @@ e1000_transmit(struct mbuf *m)
   // the TX descriptor ring so that the e1000 sends it. Stash
   // a pointer so that it can be freed after sending.
   //
-  
+  acquire(&e1000_lock_tx);
+  // 在环形队列中获取发送描述符位置
+  uint idx = regs[E1000_TDT] % TX_RING_SIZE;
+  struct tx_desc *desc = &tx_ring[idx];
+  if (desc->status & E1000_TXD_STAT_DD) {
+    // 发送描述符对应的mbuf
+    if (tx_mbufs[idx]) {
+      mbuffree(tx_mbufs[idx]);
+    }
+    desc->addr = (uint64)m->head;
+    desc->length = m->len;
+    desc->cmd = E1000_TXD_CMD_RS | E1000_TXD_CMD_EOP;
+    tx_mbufs[idx] = m;
+    regs[E1000_TDT] = (idx + 1) % TX_RING_SIZE;
+  } else {
+    goto fail;
+  }
+  release(&e1000_lock_tx);
   return 0;
+fail:
+  release(&e1000_lock_tx);
+  printf("e1000 tx error.\n");
+  return -1;
 }
 
-static void
-e1000_recv(void)
-{
+static void e1000_recv(void) {
   //
   // Your code here.
   //
   // Check for packets that have arrived from the e1000
   // Create and deliver an mbuf for each packet (using net_rx()).
   //
+  acquire(&e1000_lock_rx);
+  while (1) {
+    uint idx = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
+    struct rx_desc *desc = &rx_ring[idx];
+    if (desc->status & E1000_RXD_STAT_DD) {
+      // 获取描述符对应的mbuf
+      rx_mbufs[idx]->len = desc->length;
+      // 将包交给处理函数，解析包头协议&路由给对应的协议栈
+      net_rx(rx_mbufs[idx]);
+      // 收尾
+      rx_mbufs[idx] = mbufalloc(0);
+      desc->addr = (uint64)rx_mbufs[idx]->head;
+      desc->status = 0;
+      regs[E1000_RDT] = idx;
+    } else {
+      goto end;
+    }
+  }
+
+end:
+  release(&e1000_lock_rx);
+  return;
 }
 
-void
-e1000_intr(void)
-{
+void e1000_intr(void) {
   // tell the e1000 we've seen this interrupt;
   // without this the e1000 won't raise any
   // further interrupts.
